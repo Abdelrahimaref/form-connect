be1cc085a94238296d951f70414b9e23740174bd
abdelrahim123 <48460743+abdelrahim123@users.noreply.github.com>
2019-11-21 04:07:22 +0200

    last_home_page

---
+++ js/style.js
@@ -0,0 +1,13 @@
+$(window).on("scroll",function(){
+    if($(window).scrollTop()>=300)
+    {
+        $('#goUPButton').slideDown(700);
+    }
+    else
+    {
+        $('#goUPButton').slideUp(700); 
+    }
+}) ;
+function goUP(){
+    window.scrollTo(0,0); 
+};
